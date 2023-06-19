c96d316e9388a9f35b978689fb31a0c712b6e0af
Rachelle Lim <lrachelle653@gmail.com>
2023-06-19 13:36:10 +0800

    Removed fragments + moved Homepage to MainActivity (fixed RecyclerView bug)

--- app/src/main/java/com/example/efficenz/MainActivity.java
+++ app/src/main/java/com/example/efficenz/MainActivity.java
@@ -1,41 +1,87 @@
 package com.example.efficenz;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
-import com.example.efficenz.model.Data;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.efficenz.databinding.ActivityMainBinding;
+import com.example.efficenz.model.Data;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 
 public class MainActivity extends AppCompatActivity {
-
-    private ActivityMainBinding binding;
+    private DatabaseReference databaseRef;
+    private RecyclerView recyclerView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
 
+        recyclerView = findViewById(R.id.taskRecycler);
+        layoutManager.setReverseLayout(false);
+        layoutManager.setStackFromEnd(false);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(layoutManager);
+
+        databaseRef = FirebaseDatabase.getInstance().getReference().child("TaskNote");
+        databaseRef.keepSynced(true);
     }
 
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Query q = databaseRef.orderByChild("timestamp").limitToLast(4);
+        FirebaseRecyclerOptions<Data> taskList = new FirebaseRecyclerOptions.Builder<Data>()
+                .setQuery(q, Data.class)
+                .build();
+
+        FirebaseRecyclerAdapter<Data, TaskViewHolder> adapter = new FirebaseRecyclerAdapter<Data, TaskViewHolder>(taskList) {
+            @Override
+            protected void onBindViewHolder(@NonNull TaskViewHolder holder, int position, @NonNull Data model) {
+                holder.setTitle(model.getTitle());
+            }
+
+            @NonNull
+            @Override
+            public TaskViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View itemView = LayoutInflater.from(parent.getContext()).inflate(android.R.layout.simple_list_item_1, parent, false);
+                return new TaskViewHolder(itemView);
+            }
+        };
+
+        recyclerView.setAdapter(adapter);
+        adapter.startListening();
+    }
+
+
+    // ViewHolder class for the RecyclerView items
+    public static class TaskViewHolder extends RecyclerView.ViewHolder {
+        View taskView;
+
+        public TaskViewHolder(@NonNull View itemView) {
+            super(itemView);
+            taskView = itemView;
+        }
+
+        public void setTitle(String title) {
+
+            TextView task = taskView.findViewById(android.R.id.text1);
+            task.setText(title);
+        }
+    }
 }
