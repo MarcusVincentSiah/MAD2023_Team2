b758c3d6276381aed56b3a738daa4b88e7d02859
Thu Ta <s10242515@connect.np.edu..sg>
2023-07-24 21:40:22 +0800

    nth

--- app/src/main/java/sg/edu/np/mad/EfficenZ/Login.java
+++ app/src/main/java/sg/edu/np/mad/EfficenZ/Login.java
@@ -1,7 +1,9 @@
 package sg.edu.np.mad.EfficenZ;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
